ba8cc0da8e9dff46870bff39f7eb4c844a714f5c
Ruby <93859705+MisterRuby@users.noreply.github.com>
2024-11-30 09:15:18 +0900

    chore : 프로젝트 초기 셋팅

---
+++ src/main/kotlin/ruby/userinteresttagbit/UserInterestTagbitApplication.kt
@@ -0,0 +1,11 @@
+package ruby.userinteresttagbit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserInterestTagbitApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserInterestTagbitApplication>(*args)
+}
---
+++ src/test/kotlin/ruby/userinteresttagbit/UserInterestTagbitApplicationTests.kt
@@ -0,0 +1,13 @@
+package ruby.userinteresttagbit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserInterestTagbitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
